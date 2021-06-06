f546fe24cc5a717f0582b51f7e2f71a68d87884b
amirhosein <amirhoseinheshmati@gmail.com>
2021-06-07 00:24:32 +0430

    How do i even go about making such a query.. I'm so confused.. i don't even know what database to use Graph might be overkill RDBMS looks like so much effort for migration and database instance and mongo looks dumb and underpowered for what i want to do

--- application/routes/jobs.ts
+++ application/routes/jobs.ts
@@ -8,5 +8,34 @@ router.get("/jobs/", async function (req, res, _) {
     (await Job.find({}).populate("skills", "name -_id").limit(10).exec()) || {}
   )
 })
+router.get("/django/", async function (req, res, _) {
+  res.send(
+    (await Job.aggregate([
+      {
+        $lookup: {
+          from: "skills",
+          // localField: "skills",
+          // foreignField: "_id",
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  // _id: "60bd222ab048b56051b623ac",
+                  name: "django",
+                  // $and: [
+                  //   { $eq: ["$_id", "$skills"] },
+                  // ],
+                },
+              },
+            },
+          ],
+          as: "enrollee_info",
+        },
+      },
+    ])
+      .limit(10)
+      .exec()) || {}
+  )
+})
 
 export default router
