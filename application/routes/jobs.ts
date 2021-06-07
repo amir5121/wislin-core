e5a96354b3cd027cdb1a04e3f0ef2b82cdbbee48
amirhosein <amirhoseinheshmati@gmail.com>
2021-06-08 01:03:33 +0430

    nothing basically.. more confused TBH but for now i'm trying to work with mongo and see where it takes me.. i

--- application/routes/jobs.ts
+++ application/routes/jobs.ts
@@ -1,41 +1,25 @@
 import express from "express"
 import Job from "../models/job"
+import Skill from "../models/skill"
 
 const router = express.Router()
 
 router.get("/jobs/", async function (req, res, _) {
   res.send(
-    (await Job.find({}).populate("skills", "name -_id").limit(10).exec()) || {}
+    (await Job.find({}).populate("skills", "name").limit(10).exec()) || {}
   )
 })
+
 router.get("/django/", async function (req, res, _) {
-  res.send(
-    (await Job.aggregate([
-      {
-        $lookup: {
-          from: "skills",
-          // localField: "skills",
-          // foreignField: "_id",
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  // _id: "60bd222ab048b56051b623ac",
-                  name: "django",
-                  // $and: [
-                  //   { $eq: ["$_id", "$skills"] },
-                  // ],
-                },
-              },
-            },
-          ],
-          as: "enrollee_info",
-        },
-      },
-    ])
-      .limit(10)
-      .exec()) || {}
-  )
+  const skill = await Skill.findOne({ name: "django" }).exec()
+  if (skill)
+    res.send(
+      (await Job.find({
+        skills: skill._id,
+      })
+        .limit(10)
+        .exec()) || {}
+    )
 })
 
 export default router
