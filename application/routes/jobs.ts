d2ee34cb9c5c788f8d2791bed7c1c801e98e0e88
amirhosein <amirhoseinheshmati@gmail.com>
2021-06-09 00:35:20 +0430

    getting somewhere

--- application/routes/jobs.ts
+++ application/routes/jobs.ts
@@ -12,14 +12,47 @@ router.get("/jobs/", async function (req, res, _) {
 
 router.get("/django/", async function (req, res, _) {
   const skill = await Skill.findOne({ name: "django" }).exec()
-  if (skill)
+  if (skill) {
+    // res.send(
+    //   (await Job.find({
+    //     $expr: {
+    //       $and: [
+    //         {
+    //           skills: skill._id,
+    //         },
+    //         // { "$eq": ["$name", "development"] },
+    //         // { "$gte": [{ "$size": "$followers" }, followers_count ]}
+    //       ],
+    //     },
+    //   })
+    //     .limit(10)
+    //     .exec()) || {}
+    // )
+    const jobsGroupedBySkill = await Job.aggregate([
+      { $match: { skills: skill._id } },
+      { $unwind: "$skills" },
+      { $group: { _id: "$skills", counts: { $sum: 1 } } },
+      { $sort: { counts: -1 } },
+    ])
+      // .limit(10)
+      .exec()
+    let skilMap: any = {}
+    await Skill.find(
+      {
+        _id: { $in: jobsGroupedBySkill.map((el: any) => el._id) },
+      },
+      "name"
+    ).map((el: any) => (skilMap[el._id] = el.name))
+    console.log("OIPOPOPO", skilMap)
+
     res.send(
-      (await Job.find({
-        skills: skill._id,
-      })
-        .limit(10)
-        .exec()) || {}
+      jobsGroupedBySkill.map((el: any) => {
+        el.name = skilMap[el._id]
+        // console.log(el)
+        return el
+      }) || {}
     )
+  }
 })
 
 export default router
--- application/routes/skills.ts
+++ application/routes/skills.ts
@@ -3,6 +3,12 @@ import Skill from "../models/skill"
 
 const router = express.Router()
 
+router.get("/skills/:skillId", async function (req, res, _) {
+  res.send(
+    (await Skill.find({ _id: req.params.skillId }).limit(100).exec()) || {}
+  )
+})
+
 router.get("/skills/", async function (req, res, _) {
   res.send((await Skill.find({}).limit(100).exec()) || {})
 })
